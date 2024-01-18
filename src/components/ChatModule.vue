f2271824d60306a874e6371d848c7b36c709c6da
dhtns818 <dhtns818@rosesystems.kr>
2024-01-18 10:06:34 +0900

    update ChatModule

--- src/components/ChatModule.vue
+++ src/components/ChatModule.vue
@@ -1,22 +1,24 @@
 <script>
-import partialParse from "partial-json-parser";
+import { useAuthStore } from '@/stores/auth';
 
-import CommonStorageBase from "@/components/storage/CommonStorageBase";
+const auth = useAuthStore();
 
 export default {
     data: () => ({
-        isInitDone: false,
-        storage: null,
         generator: null,
         messages: [],
         userInfo: {},
+        disableChat: false,
+        // tests: {},
+        // testEnabled: false
     }),
     methods: {
         async init() {
-            var me = this
-            console.log(me.path)
-            me.storage = new CommonStorageBase(this);
-            me.userInfo = await me.storage.getUserInfo();
+            this.disableChat = false;
+            this.userInfo = auth.storage.userInfo;
+            
+            await this.loadData(this.getDataPath());
+            await this.loadMessages(this.getDataPath());
         },
 
         async getChatList(){
@@ -59,51 +61,54 @@ export default {
                 this.messages = messages.reverse().concat(this.messages);
             }
         },
+
+        getDataPath(){
+            return this.$route.href.replace("#/", "");
+        },
+
+        async loadData(path){
+        },
+
+        runTest(){
+            if(this.tests){
+                Object.values(tests).forEach(test => test(this))
+            }
+        },
     
         async loadMessages(path) {
-            let value;
-            if (path) {
-                value = await this.storage.getObject(`db://${path}`);
-            } else {
-                value = await this.storage.getObject(`db://${this.path}`);
-            }
-    
-            if (value && value.messages) {
-                return partialParse(value.messages);
-            } else {
-                return this.messages;
-            }
+            const callPath = path ? path : this.path;
+            await auth.storage.watch(`db://${callPath}`, (callback) => {
+                if (callback) {
+                    if (callback.messages) {
+                        this.messages = callback.messages;
+                    } else {
+                        this.messages = [];
+                    }
+                }
+            });
         },
 
         async getData(path) {
             let value;
             if (path) {
-                value = await this.storage.getObject(`db://${path}`);
+                value = await auth.storage.getObject(`db://${path}`);
             } else {
-                value = await this.storage.getObject(`db://${this.path}`);
+                value = await auth.storage.getObject(`db://${this.path}`);
             }
             return value;
         },
     
         async sendMessage(message) {
             if (message !== "") {
-                let messages = []
-                this.messages.forEach(function (msg){
-                    messages.push({
-                        role: msg.role,
-                        content: msg.content
-                    })
-                })
-
                 const chatObj = {
                     role: "user",
                     content: message
                 }
-                messages.push(chatObj);
+                this.messages.push(chatObj);
 
                 this.generator.previousMessages = [
                     ...this.generator.previousMessages,
-                    ...messages
+                    ...this.messages
                 ];
     
                 await this.generator.generate();
@@ -116,7 +121,7 @@ export default {
             }
         },
 
-        async editSendMessage(index) {
+        async sendEditedMessage(index) {
             if (index) {
                 this.messages.splice(index);
 
@@ -134,9 +139,40 @@ export default {
                 });
             }
         },
+
+        sendNotification(uid, obj) {
+            const path = `users/${uid}/notifications`;
+            this.pushObject(path, obj);
+        },
     
-        async saveMessages(path, obj) {
-            await this.storage.putObject(`db://${path}`, obj);
+        async putObject(path, obj) {
+            await auth.storage.putObject(`db://${path}`, obj);
+        },
+
+        async pushObject(path, obj) {
+            await auth.storage.pushObject(`db://${path}`, obj);
+        },
+
+        async setObject(path, obj) {
+            await auth.storage.setObject(`db://${path}`, obj);
+        },
+
+        async delete(path) {
+            await auth.storage.delete(`db://${path}`);
+        },
+
+        async getUid(email) {
+            let uid = "";
+            const userList = await this.getData("users");
+            if (userList) {
+                const ids = Object.keys(userList);
+                ids.forEach(id => {
+                    if (userList[id].email == email) {
+                        uid = id;
+                    }
+                });
+            }
+            return uid;
         },
     
         onModelCreated(response) {
@@ -146,23 +182,12 @@ export default {
             this.afterModelCreated(response);
         },
     
-        onGenerationFinished(response) {
+        onGenerationFinished(responses) {
+            // console.log(responses);
             let messageWriting = this.messages[this.messages.length -1];
             delete messageWriting.isLoading;
     
-            var msgText = "";
-            if (this.messages) {
-                msgText = JSON.stringify(this.messages);
-            }
-    
-            var putObj =  {
-                messages: msgText,
-            }
-    
-            this.afterGenerationFinished(putObj);
-            if(this.pushMessage){
-                this.pushMessage(response, 'system');
-            }
+            this.afterGenerationFinished();
         },
     
         onError(error) {
@@ -174,18 +199,72 @@ export default {
                 
             } else {
                 let messageWriting = this.messages[this.messages.length -1];
-                delete messageWriting.isLoading;
-                messageWriting.content = error.message;
+                if (messageWriting.role =="system" && messageWriting.isLoading) {
+                    delete messageWriting.isLoading;
+                    messageWriting.content = error.message;
+                } else {
+                    this.messages.push({
+                        role: "system",
+                        content: error.message,
+                    });
+                }
             }
         },
-        extractProcessJson(text) {            
-            let textAndJson = text.split("--- json ---")
-            if(textAndJson && textAndJson.length==2) return textAndJson[1]
+
+        checkDisableChat(value) {
         },
-        extractJSON(text) {            
-            const regex = /```json\s*([\s\S]*?)(?:\n\s*```|$)/;
-            const match = text.match(regex);
-            return match ? match[1].trim() : null;
+
+        extractProcessJson(text) {            
+            let textAndJson = text.split("--- json ---");
+            if(textAndJson && textAndJson.length==2) {
+                return textAndJson[1];
+            }
+        },
+
+        hasUnclosedTripleBackticks(inputString) {
+            // 백틱 세 개의 시작과 끝을 찾는 정규 표현식
+            const regex = /`{3}/g;
+            let match;
+            let isOpen = false;
+
+            // 모든 백틱 세 개의 시작과 끝을 찾습니다
+            while ((match = regex.exec(inputString)) !== null) {
+                // 현재 상태를 토글합니다 (열림 -> 닫힘, 닫힘 -> 열림)
+                isOpen = !isOpen;
+            }
+
+            // 마지막으로 찾은 백틱 세 개가 닫혀있지 않은 경우 true 반환
+            return isOpen;
+        },
+
+        extractJSON(inputString, checkFunction) {
+            try{
+                JSON.parse(inputString) // if no problem, just return the whole thing
+                return inputString
+            }catch(e){}
+
+            if(this.hasUnclosedTripleBackticks(inputString)){
+                inputString = inputString + "\n```"
+            }
+
+            // 정규 표현식 정의
+            const regex = /^.*?`{3}(?:json)?\n(.*?)`{3}.*?$/s;
+
+            // 정규 표현식을 사용하여 입력 문자열에서 JSON 부분 추출
+            const match = inputString.match(regex);
+
+            // 매치된 결과가 있다면, 첫 번째 캡쳐 그룹(즉, JSON 부분)을 반환
+            if (match) {
+                if(checkFunction)
+                    match.forEach(shouldBeJson=>{
+                        if(checkFunction(shouldBeJson)) return shouldBeJson
+                    })
+                else    
+                    return match[1];
+            }
+
+            // 매치된 결과가 없으면 null 반환
+            return null;
         },
         extractXML(text) {            
             const regex = /```xml\s*([\s\S]*?)(?:\n\s*```|$)/;
@@ -213,6 +292,10 @@ export default {
             return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
                 s4() + '-' + s4() + s4() + s4();
         },
+
+        createTest(){
+            return null
+        }
     },
 }
 </script>
