88ce14151046f97298721b469ef335036dd833c4
jyjang <jyjang@uengine.org>
2024-01-22 13:59:56 +0900

    cache Steps

--- src/components/ai/AIGenerator.js
+++ src/components/ai/AIGenerator.js
@@ -36,6 +36,8 @@ export default class AIGenerator {
             }
         }
 
+        this.cacheReplayDelay = this.options.cacheReplayDelay ? this.options.cacheReplayDelay: 100
+
     }
     
     createPrompt(){
@@ -60,13 +62,9 @@ export default class AIGenerator {
         return hash;
     }
 
+    returnCache(messages){
 
-    async generate(){
-
-        this.state = 'running'
-        let me = this;
-        let messages = this.createMessages();
-
+        let me = this
 
         if(localStorage.getItem("useCache")=="true"){
             let message = JSON.stringify(messages)
@@ -75,26 +73,94 @@ export default class AIGenerator {
             let existingResult = localStorage.getItem("cache-" + hashKey)
 
             if(existingResult){
-                setTimeout(()=>{
+                let localStorageKey = "cache-" + hashKey + "-steps"
+                let steps = localStorage.getItem("cache-" + hashKey + "-steps")
 
-                    me.state = 'end';
+                if(steps){
+                    steps = JSON.parse(steps)
 
-                    let model = me.createModel(existingResult)
-        
-                    if(me.client.onModelCreated){
-                        me.client.onModelCreated(model);
-                    } 
-                    
-                    if(me.client.onGenerationFinished)
-                        me.client.onGenerationFinished(model)
-        
+                    let i=0
 
-                }, 0)
+                    let loop = setInterval(()=>{
+    
+                        let model = me.createModel(existingResult.substring(0, steps[i++]))
+            
+                        if(me.client.onModelCreated){
+                            me.client.onModelCreated(model);
+                        } 
 
-                return
+                        if(steps.length -1 == i){ //last
+
+                            me.state = 'end';
+
+                            if(me.client.onGenerationFinished)
+                                me.client.onGenerationFinished(model)
+
+                            clearInterval(loop);
+                        }
+    
+                    }, me.cacheReplayDelay)
+                }else
+                
+                    setTimeout(()=>{
+
+                        me.state = 'end';
+
+                        let model = me.createModel(existingResult)
+            
+                        if(me.client.onModelCreated){
+                            me.client.onModelCreated(model);
+                        } 
+                        
+                        if(me.client.onGenerationFinished)
+                            me.client.onGenerationFinished(model)
+
+                    }, 0)
+
+                return true
             }
 
         }
+
+    }
+
+    saveCacheSteps(messages, step){
+        let me = this
+        let hashKey = me.generateHashKey(JSON.stringify(messages))
+
+        //save the result lengths of step-by-step
+        if(localStorage.getItem("useCache")=="true"){
+            let localStorageKey = "cache-" + hashKey + "-steps"
+            let steps = localStorage.getItem("cache-" + hashKey + "-steps")
+            if(steps){
+                steps = JSON.parse(steps)
+            }else  
+                steps = []
+
+            steps.push(step)
+
+            localStorage.setItem(localStorageKey, JSON.stringify(steps))    
+        }        
+    }
+
+    saveCache(messages){
+        let me = this
+
+        let hashKey = me.generateHashKey(JSON.stringify(messages))
+
+        if(localStorage.getItem("useCache")=="true"){
+            localStorage.setItem("cache-" + hashKey, me.modelJson)
+        }
+    }
+
+    async generate(){
+
+        this.state = 'running'
+        let me = this;
+        let messages = this.createMessages();
+
+        if(this.returnCache(messages))
+            return
 
         me.openaiToken = me.getToken();
         let responseCnt = 0;
@@ -106,64 +172,67 @@ export default class AIGenerator {
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.setRequestHeader("Authorization", "Bearer " + me.openaiToken);
 
-        // xhr.onprogress = function(event) {
-        //     var currentResId
-        //     if(me.stopSignaled){
-        //         xhr.abort();
-        //         me.stopSignaled = false;
-        //         me.state = 'stopped'
-        //     }
-        //     // console.log("Received " + event.loaded + " bytes of data.");
-        //     // console.log("Data: " + xhr.responseText);
-        //     const newUpdates = xhr.responseText
-        //     .replace("data: [DONE]", "")
-        //     .trim()
-        //     .split('data: ')
-        //     .filter(Boolean)
+        xhr.onprogress = function(event) {
+            var currentResId
+            if(me.stopSignaled){
+                xhr.abort();
+                me.stopSignaled = false;
+                me.state = 'stopped'
+            }
+            // console.log("Received " + event.loaded + " bytes of data.");
+            // console.log("Data: " + xhr.responseText);
+            const newUpdates = xhr.responseText
+            .replace("data: [DONE]", "")
+            .trim()
+            .split('data: ')
+            .filter(Boolean)
 
-        //     const newUpdatesParsed = newUpdates.map((update) => {
-        //         const parsed = JSON.parse(update);
+            const newUpdatesParsed = newUpdates.map((update) => {
+                const parsed = JSON.parse(update);
 
-        //         if(parsed.error){
-        //             if(me.client.onError){
-        //                 me.client.onError(parsed.error);
-        //             }
-        //             throw new Error(parsed.error.message)
-        //         }
+                if(parsed.error){
+                    if(me.client.onError){
+                        me.client.onError(parsed.error);
+                    }
+                    throw new Error(parsed.error.message)
+                }
 
-        //         currentResId = parsed.id
-        //         if(!me.gptResponseId){
-        //             me.gptResponseId = parsed.id
-        //         } 
-        //         if(parsed.choices[0].finish_reason == 'length'){
-        //             me.finish_reason = 'length'
-        //         }
-        //         return parsed.choices[0].delta.content || '';
-        //     });
+                currentResId = parsed.id
+                if(!me.gptResponseId){
+                    me.gptResponseId = parsed.id
+                } 
+                if(parsed.choices[0].finish_reason == 'length'){
+                    me.finish_reason = 'length'
+                }
+                return parsed.choices[0].delta.content || '';
+            });
 
-        //     const newUpdatesJoined = newUpdatesParsed.join('')
-        //     if(newUpdatesJoined.includes(": null")){
-        //         newUpdatesJoined.replaceAll(": null", ": 'null'")
-        //     }
-        //     me.modelJson = newUpdatesJoined
+            const newUpdatesJoined = newUpdatesParsed.join('')
+            if(newUpdatesJoined.includes(": null")){
+                newUpdatesJoined.replaceAll(": null", ": 'null'")
+            }
+            me.modelJson = newUpdatesJoined
 
-        //     if(me.client.onReceived){
-        //         if(me.gptResponseId == currentResId){
-        //             me.client.onReceived(newUpdatesJoined);
-        //         }
-        //     }
+            if(me.client.onReceived){
+                if(me.gptResponseId == currentResId){
+                    me.client.onReceived(newUpdatesJoined);
+                }
+            }
 
-        //     if(me.client.onModelCreated){
-        //         if(responseCnt > 15){
-        //             me.client.onModelCreated(me.createModel(newUpdatesJoined));
-        //             responseCnt = 0;
-        //         } else {
-        //             responseCnt++;
-        //         }
-        //     }
+            if(me.client.onModelCreated){
+                if(responseCnt > 15){
+
+                    me.saveCacheSteps(messages, newUpdatesJoined.length)
+
+                    me.client.onModelCreated(me.createModel(newUpdatesJoined));
+                    responseCnt = 0;
+                } else {
+                    responseCnt++;
+                }
+            }
 
 
-        // };
+        };
 
         xhr.onloadend = function() {
             console.log("End to Success - onloadend", xhr);
@@ -183,10 +252,7 @@ export default class AIGenerator {
                 if(me.client.onGenerationFinished)
                     me.client.onGenerationFinished(model)
 
-                if(localStorage.getItem("useCache")=="true"){
-                    let hashKey = me.generateHashKey(JSON.stringify(messages))
-                    localStorage.setItem("cache-" + hashKey, me.modelJson)
-                }
+                me.saveCache(messages)
             
             }
 
