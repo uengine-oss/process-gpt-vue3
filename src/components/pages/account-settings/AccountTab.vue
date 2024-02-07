03ca66b292d734f9f399d62a0a3932ff4243e7c4
kibumpark <top5w5@naver.com>
2024-02-07 13:39:13 +0900

    accountTab 관련 한/영 적용

--- src/components/pages/account-settings/AccountTab.vue
+++ src/components/pages/account-settings/AccountTab.vue
@@ -62,7 +62,7 @@ const showConfirmPwd = ref(false);
             <v-col cols="12" sm="6">
                 <v-card elevation="10">
                     <v-card-item>
-                        <h5 class="text-h5">프로필 이미지 변경</h5>
+                        <h5 class="text-h5">{{ $t('accountTab.profileImageChange') }}</h5>
                         <div class="text-center mt-6 mb-6">
                             <v-avatar size="120">
                                 <img :src="selectedProfileImage || picture || ''" height="120" alt="image" />
@@ -71,7 +71,7 @@ const showConfirmPwd = ref(false);
                         <div class="d-flex justify-center">
                             <v-dialog width="650" v-model="imageChangeDialog">
                                 <template v-slot:activator="{ props }">
-                                    <v-btn v-bind="props" color="primary" class="mx-2" rounded="pill" text="이미지 선택">이미지 선택</v-btn>
+                                    <v-btn v-bind="props" color="primary" class="mx-2" rounded="pill" text="이미지 선택">{{ $t('accountTab.imageSlect') }}</v-btn>
                                 </template>
 
                                 <v-card style="padding:30px;">
@@ -93,17 +93,17 @@ const showConfirmPwd = ref(false);
                             </v-dialog>
                             <!-- <v-btn color="error" class="mx-2" variant="outlined" rounded="pill">Reset</v-btn> -->
                         </div>
-                        <div class="text-subtitle-1 text-grey100 text-center my-sm-8 my-6">JPG, GIF 또는 PNG가 허용됩니다. 최대 크기 800K</div>
+                        <div class="text-subtitle-1 text-grey100 text-center my-sm-8 my-6">{{ $t('accountTab.slectImageExplanation') }}</div>
                     </v-card-item>
                 </v-card>
             </v-col>
             <v-col cols="12" sm="6">
                 <v-card elevation="10">
                     <v-card-item>
-                        <h5 class="text-h5">비밀번호 변경</h5>
-                        <div class="text-subtitle-1 text-grey100 mt-2">비밀번호를 변경하려면 여기에서 확인하세요.</div>
+                        <h5 class="text-h5">{{ $t('accountTab.changePassword') }}</h5>
+                        <div class="text-subtitle-1 text-grey100 mt-2">{{ $t('accountTab.changePasswordExplanation') }}</div>
                         <div class="mt-5">
-                            <v-label class="mb-2 font-weight-medium">현재 비밀번호</v-label>
+                            <v-label class="mb-2 font-weight-medium">{{ $t('accountTab.currentPassword') }}</v-label>
                             <v-text-field
                                 color="primary"
                                 variant="outlined"
@@ -113,7 +113,7 @@ const showConfirmPwd = ref(false);
                                 @click:append="showCurrentPwd = !showCurrentPwd"
                             />
 
-                            <v-label class="mb-2 font-weight-medium">새 비밀번호</v-label>
+                            <v-label class="mb-2 font-weight-medium">{{ $t('accountTab.newPassword') }}</v-label>
                             <v-text-field
                                 color="primary"
                                 variant="outlined"
@@ -123,7 +123,7 @@ const showConfirmPwd = ref(false);
                                 @click:append="showNewPwd = !showNewPwd"
                             />
 
-                            <v-label class="mb-2 font-weight-medium">비밀번호 확인</v-label>
+                            <v-label class="mb-2 font-weight-medium">{{ $t('accountTab.confirmPassword') }}</v-label>
                             <v-text-field
                                 color="primary"
                                 variant="outlined"
@@ -140,12 +140,12 @@ const showConfirmPwd = ref(false);
             <v-col cols="12">
                 <v-card elevation="10">
                     <v-card-item>
-                        <h5 class="text-h5">개인정보</h5>
-                        <div class="text-subtitle-1 text-grey100 mt-2">개인 세부정보를 변경하려면 여기에서 수정하고 저장하세요.</div>
+                        <h5 class="text-h5">{{ $t('accountTab.personalDetails') }}</h5>
+                        <div class="text-subtitle-1 text-grey100 mt-2">{{ $t('accountTab.personalDetailsExplanation') }}</div>
                         <div class="mt-5">
                             <v-row>
                                 <v-col cols="12" md="6">
-                                     <v-label class="mb-2 font-weight-medium">이름</v-label>
+                                     <v-label class="mb-2 font-weight-medium">{{ $t('accountTab.name') }}</v-label>
                                         <v-text-field
                                             color="primary"
                                             variant="outlined"
@@ -183,7 +183,7 @@ const showConfirmPwd = ref(false);
                                         ></v-select>
                                 </v-col> -->
                                 <v-col cols="12" md="6">        
-                                        <v-label class="mb-2 font-weight-medium">이메일</v-label>
+                                        <v-label class="mb-2 font-weight-medium">{{ $t('accountTab.email') }}</v-label>
                                         <v-text-field
                                             color="primary"
                                             variant="outlined"
@@ -193,7 +193,7 @@ const showConfirmPwd = ref(false);
                                         ></v-text-field>
                                 </v-col>
                                 <v-col cols="12" md="6">        
-                                        <v-label class="mb-2 font-weight-medium">전화번호</v-label>
+                                        <v-label class="mb-2 font-weight-medium">{{ $t('accountTab.phone') }}</v-label>
                                         <v-text-field
                                             color="primary"
                                             variant="outlined"
@@ -203,7 +203,7 @@ const showConfirmPwd = ref(false);
                                         ></v-text-field>
                                 </v-col>
                                 <v-col cols="12">        
-                                        <v-label class="mb-2 font-weight-medium">주소</v-label>
+                                        <v-label class="mb-2 font-weight-medium">{{ $t('accountTab.address') }}</v-label>
                                         <v-text-field
                                             color="primary"
                                             variant="outlined"
@@ -221,7 +221,7 @@ const showConfirmPwd = ref(false);
         <div class="d-flex justify-end mt-5 pb-3">
             <v-btn @click="saveUserProfile()"
                 size="large" color="primary" rounded="pill" class="mr-4"
-            >저장
+            >{{ $t('accountTab.save') }}
             </v-btn>
             <!-- <v-btn size="large" class="bg-lighterror text-error"  rounded="pill">닫기</v-btn> -->
         </div>
