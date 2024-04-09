6065825a01877bde7a5a68cc1a4c38e77a546090
ShinSeongJin2 <ShinSeongJin@uengine.org>
2024-04-09 15:19:10 +0900

    Feat: 라벨, submit html Vue 컴포넌트화 작업

---
+++ src/components/ui/LabelField.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <label :for="for_id"><slot></slot></label>
+    </div>
+</template>
+
+<script>
+
+export default {
+    components: {
+       
+    },
+    mixins: [
+        
+    ],
+    props: {
+        for_id: String
+    },
+    data() {
+        return {
+         
+        };
+    },
+    created() {
+    },
+    methods: {
+ 
+    }
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/ui/SubmitField.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <input type="submit" value="Submit">
+    </div>
+</template>
+
+<script>
+
+export default {
+    components: {
+       
+    },
+    mixins: [
+        
+    ],
+    props: {
+    },
+    data() {
+        return {
+         
+        };
+    },
+    created() {
+    },
+    methods: {
+ 
+    }
+};
+</script>
+
+<style lang="scss">
+
+</style>
