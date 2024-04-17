73175989990195e73455111e3cb7b269b0a5a0f2
Administrator <admin@example.com>
2024-04-17 14:38:11 +0900

    todolist commit

--- src/components/apps/todolist/TodoTaskColumn.vue
+++ src/components/apps/todolist/TodoTaskColumn.vue
@@ -5,36 +5,18 @@
                 <h6 class="text-h6 font-weight-semibold">{{ column.title }}</h6>
             </div>
 
-            <draggable 
-                v-if="column.id !='DONE'"
-                class="dragArea list-group cursor-move" 
-                :list="column.tasks" 
-                :animation="200" 
-                ghost-class="ghost-card"
-                group="tasks"
-                @end="updateTask"
-            >
+            <draggable v-if="column.id != 'DONE'" class="dragArea list-group cursor-move" :list="column.tasks"
+                :animation="200" ghost-class="ghost-card" group="tasks" @end="updateTask"
+                :component-data="getComponentData()">
                 <transition-group>
-                    <div v-for="task in column.tasks"
-                        :key="task.id" 
-                        class="mt-6 cursor-move"
-                    >
-                        <TodoTaskItemCard 
-                            :task="task" 
-                            @executeTask="executeTask"
-                        />
+                    <div v-for="task in column.tasks" :key="task.id" class="mt-6 cursor-move">
+                        <TodoTaskItemCard :task="task" @executeTask="executeTask" @add="addTask" />
                     </div>
                 </transition-group>
             </draggable>
             <div v-else>
-                <div v-for="task in column.tasks"
-                    :key="task.id" 
-                    class="mt-6"
-                >
-                    <TodoTaskItemCard 
-                        :task="task" 
-                        @executeTask="executeTask"
-                    />
+                <div v-for="task in column.tasks" :key="task.id" class="mt-6">
+                    <TodoTaskItemCard :task="task" @executeTask="executeTask" />
                 </div>
             </div>
         </div>
@@ -56,8 +38,10 @@ export default {
     data: () => ({
     }),
     methods: {
+        addTask(task) {
+            console.log(task)
+        },
         updateTask(event) {
-            console.log(event)
         },
         /**
          * 할 일 목록 카드의 메뉴에서 제거 버튼을 눌렀을 경우, 매칭되는 할 일을 삭제시키기 위해서
@@ -66,10 +50,24 @@ export default {
         deleteTask(task) {
             const storage = StorageBaseFactory.getStorage();
             this.column.tasks = this.column.tasks.filter((item) => item.id !== task.id);
-            storage.delete(`todolist/${task.id}`, {key: 'id'});
+            storage.delete(`todolist/${task.id}`, { key: 'id' });
         },
-        executeTask(task){
+        executeTask(task) {
             this.$emit('executeTask', task)
+        },
+        handleChange() {
+            console.log('changed');
+        },
+        inputChanged(value) {
+            console.log(value);
+        },
+        getComponentData() {
+            return {
+                onChange: this.handleChange,
+                onInput: this.inputChanged,
+                wrap: true,
+                value: this.activeNames
+            };
         }
     }
 }
