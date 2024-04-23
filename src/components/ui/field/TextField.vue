b87a742167f7694b8aa6eeab2c36a040c86f5374
ShinSeongJin2 <ShinSeongJin@uengine.org>
2024-04-23 12:53:38 +0900

    Refactor: 동일한 설정 속성 정보를 공통으로 관리하게 만듬

--- src/components/ui/field/TextField.vue
+++ src/components/ui/field/TextField.vue
@@ -11,6 +11,8 @@
 </template>
 
 <script>
+import { commonSettingInfos } from "./CommonSettingInfos.vue"
+
 export default {
     name: "TextField",
     
@@ -30,38 +32,9 @@ export default {
             localName: this.name,
             localAlias: this.alias,
 
-            /**
-             * 사용되는 속성들을 종합적으로 관리하기 위해서
-             * dataToUse: 실제로 설정이나 HTML 변수로 반영시킬 data 변수명
-             * htmlAttribute: HTML 속성으로 사용되는 이름
-             * settingLabel: 변수 값 설정창에서 표시되는 라벨
-             * settingType: 설정창에서 입력을 받기 위한 타입
-             * validCheck: 설정창에서 입력 완료시에 유효성을 확인하는데 사용되는 함수
-             */
             settingInfos: [
-                {
-                    dataToUse: "localName",
-                    htmlAttribute: "name",
-                    settingLabel: "Name",
-                    settingType: "text",
-                    validCheck: (value) => {
-                        if(!value || value.length <= 0) return "Name 속성에 값을 입력해 주세요."
-                        if(!/^[가-힣a-zA-Z0-9_\-. ]+$/.test(value)) return "Name 속성은 한글, 영문, 숫자, 공백, 밑줄(_), 대시(-), 점(.) 만 입력 가능합니다!"
-                        return null
-                    }
-                },
-
-                {
-                    dataToUse: "localAlias",
-                    htmlAttribute: "alias",
-                    settingLabel: "Label",
-                    settingType: "text",
-                    validCheck: (value) => {
-                        if(!value || value.length <= 0) return "Label 속성에 값을 입력해 주세요."
-                        if(!/^[가-힣a-zA-Z0-9_\-. ]+$/.test(value)) return "Label 속성은 한글, 영문, 숫자, 공백, 밑줄(_), 대시(-), 점(.) 만 입력 가능합니다!"
-                        return null
-                    }
-                }
+                commonSettingInfos["localName"],
+                commonSettingInfos["localAlias"]
             ]
         };
     },
