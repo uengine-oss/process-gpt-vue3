bf041635d432e37d88cb796148ac850eea7b3c9b
ShinSeongJin2 <ShinSeongJin@uengine.org>
2024-04-23 12:13:46 +0900

    Refactor: Text-Field에서 설정 및 사용할 속성을 직접 지정할 있도록 만듬

--- src/components/designer/modeling/FormDefinitionPanel.vue
+++ src/components/designer/modeling/FormDefinitionPanel.vue
@@ -1,335 +1,86 @@
 <template>
-    <v-card
-    class="mx-auto my-8"
-    elevation="16"
-    max-width="600"
-    >
-        <v-card-item>
-        <v-card-title>
-            Edit Form Attribute
-        </v-card-title>
-        <v-card-subtitle>
-            type: {{ value.type }}
-        </v-card-subtitle>
-        <v-btn icon style="position:absolute; right:5px; top:5px;" @click="$emit('onClose')">
-            <v-icon>mdi-close</v-icon>
-        </v-btn>
-        </v-card-item>
+  <v-card class="mx-auto my-8" elevation="16" max-width="600">
+    <v-card-item>
+      <v-card-title>
+          Edit Form Attribute
+      </v-card-title>
 
-        <v-card-text>
-            <v-text-field v-if="value.name !== undefined" ref="inputName" label="Name" v-model.trim="value.name"
-                          :rules="[v => !!v || 'Name is required']" required @keyup.enter="save" persistent-placeholder @input="onInputName"></v-text-field>
-            <v-text-field v-if="value.alias !== undefined" ref="inputAlias" label="Label" v-model.trim="value.alias" @keyup.enter="save"
-                          :placeholder="placeholder.alias" persistent-placeholder></v-text-field>
-            <v-text-field v-if="value.label !== undefined" ref="inputLabel" label="Label" v-model.trim="value.label"
-                          :rules="[v => !!v || 'Label is required']" required @keyup.enter="save" persistent-placeholder></v-text-field>
-        </v-card-text>
+      <v-card-subtitle>
+          type: {{ componentRef.tagName }}
+      </v-card-subtitle>
+      
+      <v-btn icon style="position:absolute; right:5px; top:5px;" @click="$emit('onClose')">
+          <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </v-card-item>
 
-        <div v-if="value.items">
-          <v-container>
-            <v-row>
-              <v-col cols="5" class="d-flex align-center justify-center">
-                KEY
-              </v-col>
-              <v-col cols="5" class="d-flex align-center justify-center">
-                VALUE
-              </v-col>
-              <v-col cols="2" class="d-flex align-center justify-center">
-              </v-col>
-            </v-row>
-            <v-divider class="my-4"></v-divider>
+    <!-- 'text' 유형의 값들을 입력받기 위해서 -->
+    <v-card-text>
+      <v-text-field v-for="(settingInfo, index) in componentRef.settingInfos.filter(info => info.settingType === 'text')" :key="index"
+                    :ref="settingInfo.dataToUse" :label="settingInfo.settingLabel" v-model.trim="componentProps[settingInfo.dataToUse]"
+                    @keyup.enter="save" persistent-placeholder></v-text-field>
+    </v-card-text>
 
-            <v-row v-for="(item, index) in value.items" :key="index">
-              <template v-for="(val, key) in item" :key="key">
-                <template v-if="index === itemIndexToEdit">
-                  <v-col cols="5" class="d-flex align-center justify-center">
-                    <v-text-field ref="inputKeyToEditItem" class="centered-input" label="Key" v-model.trim="keyToEdit"
-                                  :rules="[v => !!v || 'Key is required']" required @keyup.enter="editItem(index)"
-                                  @input="onInputKeyToEdit" persistent-placeholder></v-text-field>
-                  </v-col>
-                  <v-col cols="5" class="d-flex align-center justify-center">
-                    <v-text-field ref="inputValueToEditItem" class="centered-input" label="Value" v-model.trim="valueToEdit" @keyup.enter="editItem(index)"
-                                  :placeholder="placeholder.valueToEdit" persistent-placeholder></v-text-field>
-                  </v-col>
-                  <v-col cols="2" class="d-flex align-center justify-center">
-                    <v-sheet class="pb-5">
-                      <v-tooltip :text="$t('uiDefinition.cancel')">
-                        <template v-slot:activator="{ props }">
-                          <v-btn icon flat @click="itemIndexToEdit = -1" v-bind="props">
-                            <v-icon style="color: red;">mdi-close</v-icon>
-                          </v-btn>
-                        </template>
-                      </v-tooltip>
-                    </v-sheet>
+    <!-- 'item' 유형의 값들을 입력받기 위해서 -->
+    <v-card-text>
+      <FormDefinitionPanelItemTable v-model="testItems"></FormDefinitionPanelItemTable>
+    </v-card-text>
 
-                    <v-sheet class="pb-5">
-                      <v-tooltip :text="$t('uiDefinition.edit')">
-                        <template v-slot:activator="{ props }">
-                          <v-btn icon flat @click="editItem(index)" v-bind="props">
-                            <PencilIcon stroke-width="1.5" size="20" class="text-primary" />
-                          </v-btn>
-                        </template>
-                      </v-tooltip>
-                    </v-sheet>
-                  </v-col>
-                </template>
-                <template v-else>
-                  <v-col cols="5" class="d-flex align-center justify-center">
-                    {{ key }}
-                  </v-col>
-                  <v-col cols="5" class="d-flex align-center justify-center">
-                    {{ val }}
-                  </v-col>
-                  <v-col cols="2" class="d-flex align-center justify-center">
-                    <v-sheet>
-                      <v-tooltip :text="$t('uiDefinition.edit')">
-                        <template v-slot:activator="{ props }">
-                          <v-btn icon flat @click="itemIndexToEdit = index; keyToEdit = key; valueToEdit = val; placeholder.valueToEdit = key" v-bind="props">
-                              <PencilIcon stroke-width="1.5" size="20" class="text-primary" />
-                          </v-btn>
-                        </template>
-                      </v-tooltip>
-                    </v-sheet>
-
-                    <v-sheet>
-                      <v-tooltip :text="$t('uiDefinition.delete')">
-                        <template v-slot:activator="{ props }">
-                          <v-btn icon flat @click="deleteItem(index)" v-bind="props">
-                              <TrashIcon stroke-width="1.5" size="20" class="text-error" />
-                          </v-btn>
-                        </template>
-                      </v-tooltip>
-                    </v-sheet>
-                  </v-col>
-                </template>
-              </template>
-            </v-row>
-
-            <v-row>
-              <v-col cols="5" class="d-flex align-center justify-center">
-                <v-text-field ref="inputKeyToAddItem" class="centered-input" label="Key" v-model.trim="keyToAdd" @keyup.enter="addItem"
-                              @input="onInputKeyToAdd" persistent-placeholder></v-text-field>
-              </v-col>
-              <v-col cols="5" class="d-flex align-center justify-center">
-                <v-text-field ref="inputValueToAddItem" class="centered-input" label="Value" v-model.trim="valueToAdd" @keyup.enter="addItem"
-                              :placeholder="placeholder.valueToAdd" persistent-placeholder></v-text-field>
-              </v-col>
-              <v-col cols="2" class="d-flex align-center justify-center pb-9">
-                <v-sheet>
-                  <v-tooltip :text="$t('uiDefinition.add')">
-                    <template v-slot:activator="{ props }">
-                        <v-btn icon flat @click="addItem" v-bind="props">
-                          <v-icon style="color: green;" size="40">mdi-plus</v-icon>
-                        </v-btn>
-                    </template>
-                  </v-tooltip>
-                </v-sheet>
-              </v-col>
-            </v-row>
-          </v-container>
-        </div>
-
-        <v-btn @click="save">
-          Apply
-        </v-btn>
-    </v-card>
+    <v-btn @click="save">
+      Apply
+    </v-btn>
+  </v-card>
 </template>
   
 <script>
+  import FormDefinitionPanelItemTable from '@/components/designer/modeling/FormDefinitionPanelItemTable.vue'
+
   export default {
     name: 'form-definition-panel',
-    mixins: [],
+    components: {
+      FormDefinitionPanelItemTable
+    },
+
     emits: [
       "onClose",
       "onSave"
     ],
     props: {
-      /** {
-        id: String,
-        type: String,
-        name: String,
-        alias: String,
-        items: Array,
-        label: String
-      }
-      */
-      value: Object 
+      componentRef: Object
     },
+
     data: () => ({
-      keyToAdd: "",
-      valueToAdd: "",
+      componentProps: {},
 
-      itemIndexToEdit: -1,
-      keyToEdit: "",
-      valueToEdit: "",
-
-      initialValue: {}, // 초기에 해당 속성을 가지고 있는 경우에만 유효성을 검사시키기 위해서
-
-      regexStr: /^[가-힣a-zA-Z0-9_\-. ]+$/,
-      regexErrorMsg: "'{{propName}}'은 한글, 영문, 숫자, 공백, 밑줄(_), 대시(-), 점(.) 만 입력 가능합니다!",
-
-      placeholder: {
-        alias: "",
-        valueToAdd: "",
-        valueToEdit: ""
-      }
+      testItems: []
     }),
-    components: {
-    },
+
     methods: {
       save() {
-        //#region 유효성 검사
-        if(this.initialValue.name && (!(this.value.name) || this.value.name.length <= 0)) {
-          alert("Name is required")
-          this.$refs.inputName.focus();
-          return
-        }
-        if(this.initialValue.name && !this.regexStr.test(this.value.name)) {
-          alert(this.regexErrorMsg.replace("{{propName}}", "Name"))
-          this.$refs.inputName.focus();
-          return
-        }
+        for(const info of this.componentRef.settingInfos) {
+          const errorMessage = info.validCheck(this.componentProps[info.dataToUse])
 
-        if(this.initialValue.label && (!(this.value.label) || this.value.label.length <= 0)) {
-          alert("Label is required")
-          this.$refs.inputLabel.focus();
-          return
-        }
-
-        if(this.initialValue.alias && this.value.alias && this.value.alias.length > 0) {
-          if(!this.regexStr.test(this.value.alias)) {
-            alert(this.regexErrorMsg.replace("{{propName}}", "Alias"))
-            this.$refs.inputAlias.focus();
-            return
-          }
-        }
-        //#endregion
-        //#region 입력값 처리
-        if(this.initialValue.alias && (!(this.value.alias) || this.value.alias.length <= 0)) {
-            this.value.alias = this.value.name
-            this.$refs.inputAlias.focus();
-        }
-        //#endregion
-
-        this.$emit('onSave', JSON.parse(JSON.stringify(this.value)))
-      },
-
-      addItem() {
-        //#region 유효성 검사
-        if(!(this.keyToAdd) || this.keyToAdd.length <= 0) {
-          alert("Key is required")
-          this.$refs.inputKeyToAddItem.focus();
-          return
-        }
-        if(!this.regexStr.test(this.keyToAdd)) {
-            alert(this.regexErrorMsg.replace("{{propName}}", "Key"))
-            this.$refs.inputKeyToAddItem.focus();
-            return
-        }
-        if(this.value.items.some(item => item.hasOwnProperty(this.keyToAdd))) {
-          alert("Key already exists")
-          this.$refs.inputKeyToAddItem.focus();
-          return
-        }
-
-        if(this.valueToAdd && this.valueToAdd.length > 0) {
-          if(!this.regexStr.test(this.valueToAdd)) {
-            alert(this.regexErrorMsg.replace("{{propName}}", "Value"))
-            this.$refs.inputValueToAddItem.focus();
-            return
-          }
-        }
-        //#endregion
-        //#region 입력값 처리
-        if(!(this.valueToAdd) || this.valueToAdd.length <= 0) {
-            this.valueToAdd = this.keyToAdd
-        }
-        //#endregion
-
-        this.value.items.push({ [this.keyToAdd]: this.valueToAdd })
-        this.keyToAdd = ""
-        this.valueToAdd = ""   
-        this.placeholder.valueToAdd = ""
-        
-        this.$nextTick(() => {
-            this.$refs.inputKeyToAddItem.focus();
-        });
-      },
-
-      editItem(itemIndexToEdit) {
-        //#region 유효성 검사
-        if(!(this.keyToEdit) || this.keyToEdit.length <= 0) {
-          alert("Key is required")
-          this.$refs.inputKeyToEditItem.focus();
-          return
-        }
-        if(!this.regexStr.test(this.keyToEdit)) {
-            alert(this.regexErrorMsg.replace("{{propName}}", "Key"))
-            this.$refs.inputKeyToEditItem.focus();
-            return
-        }
-
-        // 키가 기존의 값과 달라진 경우에만 중복 여부를 검사하기 위해서
-        if(!(this.value.items[itemIndexToEdit].hasOwnProperty(this.keyToEdit))) {
-          if(this.value.items.some(item => item.hasOwnProperty(this.keyToEdit))) {
-            alert("Key already exists")
-            this.$refs.inputKeyToEditItem.focus();
+          if(errorMessage) {
+            alert(errorMessage)
+            this.$refs[info.dataToUse][0].focus()
             return
           }
         }
 
-        if(this.valueToEdit && this.valueToEdit.length > 0) {
-          if(!this.regexStr.test(this.valueToEdit)) {
-            alert(this.regexErrorMsg.replace("{{propName}}", "Value"))
-            this.$refs.inputValueToEditItem.focus();
-            return
-          }
-        }
-        //#endregion
-        //#region 입력값 처리
-        if(!(this.valueToEdit) || this.valueToEdit.length <= 0) {
-            this.valueToEdit = this.keyToEdit
-        }
-        //#endregion
-
-        this.value.items.splice(itemIndexToEdit, 1, { [this.keyToEdit]: this.valueToEdit })
-
-        this.itemIndexToEdit = -1
-        this.keyToEdit = ""
-        this.valueToEdit = ""
-        this.placeholder.valueToEdit = ""
-      },
-
-      deleteItem(itemIndexToDelete) {
-        this.value.items.splice(itemIndexToDelete, 1)
-      },
-
-
-      onInputName() {
-        if(this.value.name.length > 0)
-            this.placeholder.alias = this.value.name
-        else
-            this.placeholder.alias = this.initialValue.name
-      },
-
-      onInputKeyToAdd() {
-        this.placeholder.valueToAdd = this.keyToAdd
-      },
-
-      onInputKeyToEdit() {
-        this.placeholder.valueToEdit = this.keyToEdit
+        this.$emit('onSave', this.componentRef, this.componentProps)
       }
     },
-    created() {
-      this.initialValue = JSON.parse(JSON.stringify(this.value))
 
-      if(this.initialValue.name)
-        this.placeholder.alias = this.initialValue.name
-    },
     mounted() {
-        this.$nextTick(() => {
-            this.$refs.inputName.focus();
-        });
+      this.componentProps = this.componentRef.settingInfos.reduce((acc, cur) => {
+          acc[cur.dataToUse] = this.componentRef[cur.dataToUse]
+          return acc
+      }, {})
+
+      this.$nextTick(() => {
+          if(Object.keys(this.$refs).length > 0)
+            this.$refs[Object.keys(this.$refs)[0]][0].focus()
+      });
     },
   }
 </script>
--- src/components/ui/TextField.vue
+++ src/components/ui/TextField.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <v-text-field v-model="inputedValue">
+        <v-text-field v-model="localModelValue">
             <template v-slot:label>
                 <span style="color:black;">
-                    {{label}}
+                    {{localLabel ?? localName}}
                 </span>
             </template>
         </v-text-field>
@@ -12,45 +12,72 @@
 
 <script>
 export default {
+    name: "TextField",
+    
     props: {
         modelValue: String,
         vueRenderUUID: String,
         tagName: String,
+
         name: String,
-        alias: String
+        label: String
     },
 
-    computed: {
-        label() {
-            if (this.localAlias) return this.localAlias
-            else if (this.localName) return this.localName
-            else return ""
-        }
-    },
-    
     data() {
         return {
+            localModelValue: this.modelValue,
+
             localName: this.name,
-            localAlias: this.alias,
-            inputedValue: ""
+            localLabel: this.label,
+
+            /**
+             * 사용되는 속성들을 종합적으로 관리하기 위해서
+             * dataToUse: 실제로 설정이나 HTML 변수로 반영시킬 data 변수명
+             * htmlAttribute: HTML 속성으로 사용되는 이름
+             * settingLabel: 변수 값 설정창에서 표시되는 라벨
+             * settingType: 설정창에서 입력을 받기 위한 타입
+             * validCheck: 설정창에서 입력 완료시에 유효성을 확인하는데 사용되는 함수
+             */
+            settingInfos: [
+                {
+                    dataToUse: "localName",
+                    htmlAttribute: "name",
+                    settingLabel: "Name",
+                    settingType: "text",
+                    validCheck: (value) => {
+                        if(!value || value.length <= 0) return "Name 속성에 값을 입력해 주세요."
+                        if(!/^[가-힣a-zA-Z0-9_\-. ]+$/.test(value)) return "Name 속성은 한글, 영문, 숫자, 공백, 밑줄(_), 대시(-), 점(.) 만 입력 가능합니다!"
+                        return null
+                    }
+                },
+
+                {
+                    dataToUse: "localLabel",
+                    htmlAttribute: "label",
+                    settingLabel: "Label",
+                    settingType: "text",
+                    validCheck: (value) => {
+                        if(!value || value.length <= 0) return "Label 속성에 값을 입력해 주세요."
+                        if(!/^[가-힣a-zA-Z0-9_\-. ]+$/.test(value)) return "Label 속성은 한글, 영문, 숫자, 공백, 밑줄(_), 대시(-), 점(.) 만 입력 가능합니다!"
+                        return null
+                    }
+                }
+            ]
         };
     },
 
     watch: {
         modelValue: {
             handler() {
-                if(this.modelValue && this.modelValue.length > 0)
-                    this.inputedValue = this.modelValue
-                else
-                    this.inputedValue = ""
+                this.localModelValue  = ((this.modelValue && this.modelValue.length > 0) ? this.modelValue : "")
             },
             deep: true,
             immediate: true
         },
 
-        inputedValue: {
+        localModelValue: {
             handler() {
-                this.$emit('update:modelValue', this.inputedValue)
+                this.$emit('update:modelValue', this.localModelValue)
             },
             deep: true,
             immediate: true
