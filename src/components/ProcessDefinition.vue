881edae26764855cf347abbb54dae34eafbde03d
Administrator <admin@example.com>
2024-04-24 14:59:14 +0900

    Process Variable 수정 Commit

--- src/components/ProcessDefinition.vue
+++ src/components/ProcessDefinition.vue
@@ -573,7 +573,7 @@ export default {
             }
         },
         updateVariable(val) {
-            this.copyProcessDefinition.data[this.editedIndex] = val;
+            this.processVariables[this.editedIndex] = val;
             this.editDialog = false;
         },
         openProcessVariables() {
