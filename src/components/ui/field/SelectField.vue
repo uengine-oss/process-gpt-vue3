f5290f97a20717b6fb2fffb141ef4902b21f0e9b
ShinSeongJin2 <ShinSeongJin@uengine.org>
2024-04-26 17:57:26 +0900

    Feat: 멀티데이터 로드 관련 프로토타입 생성

--- src/components/ui/field/SelectField.vue
+++ src/components/ui/field/SelectField.vue
@@ -16,6 +16,8 @@
 
 <script>
 import { commonSettingInfos } from "./CommonSettingInfos.vue"
+import axios from 'axios';
+import jp from 'jsonpath';
 
 export default {
     props: {
@@ -31,19 +33,27 @@ export default {
 
     computed: {
         localKeys() {
-            if(this.localItems === undefined || this.localItems === null || this.localItems.length === 0) return []
-            return this.localItems.map(item => Object.keys(item)[0])
+            if(this.controlItems === undefined || this.controlItems === null ||
+               this.controlItems.length === 0 || this.controlItems === "[]") return []
+            return this.controlItems.map(item => Object.keys(item)[0])
         }
     },
 
     data() {
         return {
-            localModelValue: this.modelValue,
+            localModelValue: "",
 
-            localName: this.name,
-            localAlias: this.alias,
-            localItems: this.items,
+            localName: "",
+            localAlias: "",
+            localItems: [],
             localDisabled: this.disabled === "true",
+
+            localIsDynamicLoad: false,
+            localDynamicLoadURL: "http://localhost:8088/api/data/1",
+            localDynamicLoadKeyJsonPath: "$.values[*].key",
+            localDynamicLoadValueJsonPath: "$.values[*].value",
+
+            controlItems: [],
 
             settingInfos: [
                 commonSettingInfos["localName"],
@@ -56,20 +66,20 @@ export default {
 
     watch: {
         modelValue: {
-            handler() {
-                this.loadLocalItems()
+            async handler() {
+                await this.loadControlItems()
 
                 if(this.modelValue && this.modelValue.length > 0)
                 {
-                    const foundItem = this.localItems.find(item => Object.keys(item)[0] === this.modelValue)
+                    const foundItem = this.controlItems.find(item => Object.keys(item)[0] === this.modelValue)
                     if(!foundItem) return
 
                     this.localModelValue = Object.keys(foundItem)[0]
                 }
                 else
                 {
-                    if(this.localItems.length > 0)
-                        this.localModelValue = Object.keys(this.localItems[0])[0]
+                    if(this.controlItems.length > 0)
+                        this.localModelValue = Object.keys(this.controlItems[0])[0]
                 }
             },
             deep: true,
@@ -82,23 +92,58 @@ export default {
             },
             deep: true,
             immediate: true
-        }
+        },
+
+        localItems: {handler() {this.loadControlItems()}, deep: true, immediate: true},
+        localDynamicLoadURL: {handler() {this.loadControlItems()}, deep: true, immediate: true},
+        localDynamicLoadKeyJsonPath: {handler() {this.loadControlItems()}, deep: true, immediate: true},
+        localDynamicLoadValueJsonPath: {handler() {this.loadControlItems()}, deep: true, immediate: true}
     },
 
     methods: {
         // 문자열로 형태로 items의 값이 전달되었을 경우, 리스트 형태로 변환해서 반영시키기 위해서
-        loadLocalItems() {
-            try {
-                if(typeof(this.items) === "string")
-                    this.localItems = JSON.parse(this.items.replace(/'/g, '"'))
-                else
-                    this.localItems = this.items
-            } catch (e) {
-                console.log("### items 파싱 에러 ###")
-                console.log(this.items.replace(/'/g, '"'))
-                console.error(e);
-            }
+        async loadControlItems() {
+            if(this.localIsDynamicLoad) {
+
+                try {
+                    const response = await axios.get(this.localDynamicLoadURL)
+                    
+                    const keys = jp.query(response.data, this.localDynamicLoadKeyJsonPath)
+                    const values = jp.query(response.data, this.localDynamicLoadValueJsonPath)
+
+                    if(keys.length !== values.length) throw new Error("keys.length != values.length")
+                    this.controlItems = keys.map((key, index) => ({ [key]: values[index] }))
+                } catch(e) {
+                    console.log("### items 동적 로드 에러 ###")
+                    console.error(e)
+                }
+
+            } else
+                this.controlItems = this.localItems
         }
+    },
+
+    created() {
+        this.localModelValue = this.modelValue
+        
+        this.localName = this.name
+        this.localAlias = this.alias
+        this.localDisabled = this.disabled === "true"
+
+
+        try {
+            if(typeof(this.items) === "string")
+                this.localItems = JSON.parse(this.items.replace(/'/g, '"'))
+            else
+                this.localItems = this.items
+        } catch (e) {
+            console.log("### items 파싱 에러 ###")
+            console.log(this.items.replace(/'/g, '"'))
+            console.error(e);
+            this.localItems = []
+        }
+
+        this.loadControlItems()
     }
 };
 </script>
