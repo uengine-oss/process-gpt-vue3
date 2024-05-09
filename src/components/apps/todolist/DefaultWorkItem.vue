4b48e973ef38e72ccd768a7051bb14f2916c3f81
Administrator <admin@example.com>
2024-05-09 16:38:00 +0900

    workItem Complete Commit

--- src/components/apps/todolist/DefaultWorkItem.vue
+++ src/components/apps/todolist/DefaultWorkItem.vue
@@ -1,69 +1,68 @@
 <template>
-    <v-row class="ma-0 pa-0 task-btn"
-        style="right:40px;"
-    >
+    <v-row class="ma-0 pa-0 task-btn" style="right: 40px">
         <v-spacer></v-spacer>
         <div>
-            <v-btn @click="completeTask()" color="#0085DB" style="color: white;" rounded>완료</v-btn>
+            <v-btn @click="completeTask()" color="#0085DB" style="color: white" rounded>완료</v-btn>
         </div>
     </v-row>
-    <div style="height:calc(100vh - 255px); padding: 20px;" >
+    <div style="height: calc(100vh - 255px); padding: 20px">
         <DefaultForm :inputItems="inputItems"></DefaultForm>
     </div>
 </template>
 
 <script>
 import BackendFactory from '@/components/api/BackendFactory';
-import DefaultForm from '@/components/designer/DefaultForm.vue'
+import DefaultForm from '@/components/designer/DefaultForm.vue';
 
-const backend = BackendFactory.createBackend()
+const backend = BackendFactory.createBackend();
 export default {
-    props:{
+    props: {
         workItem: {
             type: Object,
             default: function () {
-                return null
-            },
+                return null;
+            }
         },
-        workItemStatus:{
+        workItemStatus: {
             type: String,
             default: function () {
-                return null
-            },
+                return null;
+            }
         }
     },
     data: () => ({
-        inputItems: null,      
+        inputItems: null
     }),
     components: {
         DefaultForm
-        
     },
     created() {
         this.init();
     },
     methods: {
-       async init(){
-            var me = this
-            if(!me.workItem.activity.parameters) me.workItem.activity.parameters = []
+        async init() {
+            var me = this;
+            if (!me.workItem.activity.parameters) me.workItem.activity.parameters = [];
             me.inputItems = me.workItem.activity.parameters
-                    .filter(item => item.direction.includes("OUT"))
-                    .map(item => ({ name: item.variable.name, value: null }));
+                .filter((item) => item.direction.includes('OUT'))
+                .map((item) => ({ name: item.variable.name, value: null }));
         },
-        async completeTask(){
-            var me = this
+        async completeTask() {
+            var me = this;
             me.$try({
                 context: me,
                 action: async () => {
-                    let parameterValues = this.inputItems.reduce((acc, item) => ({...acc, [item.name]: item.value}), {});
-                    await backend.putWorkItemComplete(me.$route.params.taskId, {"parameterValues": parameterValues})
-                    me.$router.push('/todolist')
+                    let workItem = { parameterValues: {} };
+                    let parameterValues = this.inputItems.reduce((acc, item) => ({ ...acc, [item.name]: item.value }), {});
+                    if (parameterValues) workItem.parameterValues = parameterValues;
+                    if (me.workItem.execScope) workItem.execScope = me.workItem.execScope;
+                    await backend.putWorkItemComplete(me.$route.params.taskId, workItem);
+
+                    me.$router.push('/todolist');
                 },
                 successMsg: '해당 업무 완료'
-            })
-
-         
-        },
-    },
-}
+            });
+        }
+    }
+};
 </script>
