903b7d22eaf87c99a935409c13f3c57bfb414ac3
sooheon45 <sooheon45@uengine.org>
2024-05-22 13:48:37 +0900

    EventSync 처리(임시)

--- src/components/designer/bpmnModeling/bpmn/panel/UserTaskPanel.vue
+++ src/components/designer/bpmnModeling/bpmn/panel/UserTaskPanel.vue
@@ -1,99 +1,108 @@
 <template>
     <div>
-        <div v-if="inputData.length > 0" style="margin-bottom: 20px">
-            <div style="margin-bottom: -8px">{{ $t('BpnmPropertyPanel.inputData') }}</div>
-            <v-row class="ma-0 pa-0">
-                <div v-for="(input, idx) in inputData" :key="idx" class="mr-2 mt-2">
-                    <v-chip v-if="input.mandatory" color="primary" variant="outlined" class="text-body-2" @click="deleteInputData(input)">
-                        {{ input.argument.text }}
-                        <CircleXIcon class="ml-2" start size="20" />
-                    </v-chip>
-                    <v-chip v-else class="text-body-2" variant="outlined" @click="deleteInputData(inputData)">
-                        {{ input.argument.text }}
-                        <CircleXIcon class="ml-2" start size="20" />
-                    </v-chip>
-                </div>
-            </v-row>
+        <v-switch v-model="isEventSyncForm" :label="isEventSyncForm ? 'Event Synchronization Form':'Form Mapping' "></v-switch>
+        
+        <div v-if="isEventSyncForm">
+            <EventSynchronizationForm v-model="copyUengineProperties.eventSynchronization" :roles="roles" :taskName="name"></EventSynchronizationForm>
         </div>
-        <div v-if="outputData.length > 0" style="margin-bottom: 20px">
-            <div style="margin-bottom: -8px">{{ $t('BpnmPropertyPanel.outputData') }}</div>
-            <v-row class="ma-0 pa-0">
-                <div v-for="(output, idx) in outputData" :key="idx" class="mr-2 mt-2">
-                    <v-chip
-                        v-if="output.mandatory"
-                        color="primary"
-                        class="text-body-2"
-                        variant="outlined"
-                        @click="deleteOutputData(output)"
-                    >
-                        {{ output.variable.name }}
-                        <CircleXIcon class="ml-2" start size="20" />
-                    </v-chip>
-                    <v-chip v-else class="text-body-2" variant="outlined" @click="deleteOutputData(output)">
-                        {{ output.variable.name }}
-                        <CircleXIcon class="ml-2" start size="20" />
-                    </v-chip>
-                </div>
-            </v-row>
-        </div>
-        <div>
-            <v-row class="ma-0 pa-0">
-                <div>Parameter Context</div>
-                <v-spacer></v-spacer>
-                <bpmn-parameter-contexts :parameter-contexts="copyUengineProperties.parameters"></bpmn-parameter-contexts>
-            </v-row>
-        </div>
-        <v-spacer></v-spacer>
-        <v-checkbox v-model="isFormActivity" label="폼 기반 업무"></v-checkbox>
-        <div v-if="isFormActivity">
-            <div>
-                <v-row cclass="ma-0 pa-0">
-                    <!-- <v-col cols="12" sm="3" class="pb-sm-3 pb-0">
-                        <v-label class=" font-weight-medium" for="hcpm">{{ $t('ProcessVariable.defaultValue') }}</v-label>
-                    </v-col> -->
-                    <v-col cols="12" sm="9">
-                        <!-- <v-autocomplete 
-                            v-model="selectedForm"
-                            :items="definition.processVariables
-                                        .filter(item => item.type === 'Form' && item.defaultValue && item.defaultValue.name && item.defaultValue.alias)
-                                        .map(item => item.defaultValue.name + '_' + item.defaultValue.alias)" 
-                            color="primary" 
-                            variant="outlined"
-                            hide-details>
-                        </v-autocomplete> -->
-                        <v-autocomplete
-                            v-model="selectedForm"
-                            :items="definition.processVariables.filter((item) => item.type === 'Form').map((item) => item.name)"
+        <div v-else>
+            <div v-if="inputData.length > 0" style="margin-bottom: 20px">
+                <div style="margin-bottom: -8px">{{ $t('BpnmPropertyPanel.inputData') }}</div>
+                <v-row class="ma-0 pa-0">
+                    <div v-for="(input, idx) in inputData" :key="idx" class="mr-2 mt-2">
+                        <v-chip v-if="input.mandatory" color="primary" variant="outlined" class="text-body-2" @click="deleteInputData(input)">
+                            {{ input.argument.text }}
+                            <CircleXIcon class="ml-2" start size="20" />
+                        </v-chip>
+                        <v-chip v-else class="text-body-2" variant="outlined" @click="deleteInputData(inputData)">
+                            {{ input.argument.text }}
+                            <CircleXIcon class="ml-2" start size="20" />
+                        </v-chip>
+                    </div>
+                </v-row>
+            </div>
+            
+            <div v-if="outputData.length > 0" style="margin-bottom: 20px">
+                <div style="margin-bottom: -8px">{{ $t('BpnmPropertyPanel.outputData') }}</div>
+                <v-row class="ma-0 pa-0">
+                    <div v-for="(output, idx) in outputData" :key="idx" class="mr-2 mt-2">
+                        <v-chip
+                            v-if="output.mandatory"
                             color="primary"
+                            class="text-body-2"
                             variant="outlined"
-                            hide-details
+                            @click="deleteOutputData(output)"
                         >
-                        </v-autocomplete>
-                    </v-col>
+                            {{ output.variable.name }}
+                            <CircleXIcon class="ml-2" start size="20" />
+                        </v-chip>
+                        <v-chip v-else class="text-body-2" variant="outlined" @click="deleteOutputData(output)">
+                            {{ output.variable.name }}
+                            <CircleXIcon class="ml-2" start size="20" />
+                        </v-chip>
+                    </div>
                 </v-row>
             </div>
             <div>
                 <v-row class="ma-0 pa-0">
-                    <v-btn text color="primary" class="my-3" @click="openFormMapper()"> Field Mapping </v-btn>
+                    <div>Parameter Context</div>
+                    <v-spacer></v-spacer>
+                    <bpmn-parameter-contexts :parameter-contexts="copyUengineProperties.parameters"></bpmn-parameter-contexts>
                 </v-row>
             </div>
-            <v-dialog
-                v-model="isOpenFieldMapper"
-                max-width="80%"
-                max-height="80%"
-                @afterLeave="$refs.formMapper && $refs.formMapper.saveFormMapperJson()"
-            >
-                <form-mapper
-                    ref="formMapper"
-                    :definition="copyDefinition"
-                    :name="name"
-                    :roles="roles"
-                    :formMapperJson="formMapperJson"
-                    :activities="activities"
-                    @saveFormMapperJson="saveFormMapperJson"
-                />
-            </v-dialog>
+            <v-spacer></v-spacer>
+            <v-checkbox v-model="isFormActivity" label="폼 기반 업무"></v-checkbox>
+            <div v-if="isFormActivity">
+                <div>
+                    <v-row cclass="ma-0 pa-0">
+                        <!-- <v-col cols="12" sm="3" class="pb-sm-3 pb-0">
+                            <v-label class=" font-weight-medium" for="hcpm">{{ $t('ProcessVariable.defaultValue') }}</v-label>
+                        </v-col> -->
+                        <v-col cols="12" sm="9">
+                            <!-- <v-autocomplete 
+                                v-model="selectedForm"
+                                :items="definition.processVariables
+                                            .filter(item => item.type === 'Form' && item.defaultValue && item.defaultValue.name && item.defaultValue.alias)
+                                            .map(item => item.defaultValue.name + '_' + item.defaultValue.alias)" 
+                                color="primary" 
+                                variant="outlined"
+                                hide-details>
+                            </v-autocomplete> -->
+                            <v-autocomplete
+                                v-model="selectedForm"
+                                :items="definition.processVariables.filter((item) => item.type === 'Form').map((item) => item.name)"
+                                color="primary"
+                                variant="outlined"
+                                hide-details
+                            >
+                            </v-autocomplete>
+                        </v-col>
+                    </v-row>
+                </div>
+                <div>
+                    <v-row class="ma-0 pa-0">
+                        <v-btn text color="primary" class="my-3" @click="openFormMapper()"> Field Mapping </v-btn>
+                    </v-row>
+                </div>
+                <v-dialog
+                    v-model="isOpenFieldMapper"
+                    max-width="80%"
+                    max-height="80%"
+                    @afterLeave="$refs.formMapper && $refs.formMapper.saveFormMapperJson()"
+                >
+                    <form-mapper
+                        ref="formMapper"
+                        :definition="copyDefinition"
+                        :name="name"
+                        :roles="roles"
+                        :formMapperJson="formMapperJson"
+                        :activities="activities"
+                        @saveFormMapperJson="saveFormMapperJson"
+                    />
+                </v-dialog>
+            </div>
         </div>
+        
         <!-- <div>
             <v-row class="ma-0 pa-0">
                 <div>{{ $t('BpnmPropertyPanel.checkPoints') }}</div>
@@ -187,12 +196,14 @@ import BpmnParameterContexts from '@/components/designer/bpmnModeling/bpmn/varia
 import FormMapper from '@/components/designer/mapper/FormMapper.vue';
 import { useBpmnStore } from '@/stores/bpmn';
 import BackendFactory from '@/components/api/BackendFactory';
+import EventSynchronizationForm from '@/components/designer/EventSynchronizationForm.vue';
 
 export default {
     name: 'user-task-panel',
     components: {
         BpmnParameterContexts,
-        FormMapper
+        FormMapper,
+        EventSynchronizationForm
     },
     props: {
         uengineProperties: Object,
@@ -207,6 +218,7 @@ export default {
     created() {},
     data() {
         return {
+            isEventSyncForm: false,
             // requiredKeyLists: {
             //     "parameters": [],
             //     "checkpoints": []
@@ -247,28 +259,32 @@ export default {
         let me = this;
 
         const store = useBpmnStore();
-        this.bpmnModeler = store.getModeler;
+        me.bpmnModeler = store.getModeler;
         if (me.role?.length > 0) {
-            this.copyUengineProperties.role = { name: me.role };
+            me.copyUengineProperties.role = { name: me.role };
         }
-        if (!this.copyUengineProperties.parameters) this.copyUengineProperties.parameters = [];
+        if (!me.copyUengineProperties.parameters) me.copyUengineProperties.parameters = [];
+        if (!me.copyUengineProperties.eventSynchronization) me.copyUengineProperties.eventSynchronization = {};
 
-        if (this.copyUengineProperties.variableForHtmlFormContext) {
-            this.isFormActivity = true;
-            this.selectedForm = this.copyUengineProperties.variableForHtmlFormContext.name;
+        if (me.copyUengineProperties.variableForHtmlFormContext) {
+            me.isFormActivity = true;
+            me.selectedForm = me.copyUengineProperties.variableForHtmlFormContext.name;
         }
 
         let mapperData = {};
-        if (!this.copyUengineProperties.mappingContext) {
-            if (!this.copyUengineProperties._type == 'org.uengine.kernel.FormActivity') {
-                this.copyUengineProperties.mappingContext = mapperData;
+        if (!me.copyUengineProperties.mappingContext) {
+            if (!me.copyUengineProperties._type == 'org.uengine.kernel.FormActivity') {
+                me.copyUengineProperties.mappingContext = mapperData;
             }
         } else {
-            this.formMapperJson = JSON.stringify(this.copyUengineProperties.mappingContext, null, 2);
+            me.formMapperJson = JSON.stringify(me.copyUengineProperties.mappingContext, null, 2);
         }
-        this.$emit('update:uEngineProperties', this.copyUengineProperties);
+        if(me.copyUengineProperties.eventSynchronization){
+            me.isEventSyncForm = true
+        }
 
-        this.copyDefinition = this.definition;
+        me.$emit('update:uEngineProperties', me.copyUengineProperties);
+        me.copyDefinition = me.definition;
     },
     computed: {
         inputData() {
