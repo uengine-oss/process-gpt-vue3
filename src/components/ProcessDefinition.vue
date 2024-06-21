66da8daf8b47ceab41106ffe9e1991278d77d111
dhtns818 <dhtns818@rosesystems.kr>
2024-06-21 15:57:35 +0900

    fix executeDialog

--- src/components/ProcessDefinition.vue
+++ src/components/ProcessDefinition.vue
@@ -176,7 +176,7 @@
         </v-dialog>
 
         <v-dialog v-model="executeDialog" max-width="80%">
-            <dry-run-process v-if="mode == 'BPMN'" :definitionId="definitionPath" @close="executeDialog = false"></dry-run-process>
+            <dry-run-process v-if="mode == 'uEngine'" :definitionId="definitionPath" @close="executeDialog = false"></dry-run-process>
             <process-execute-dialog v-else :definitionId="definitionPath" :roles="roles" @close="executeDialog = false"></process-execute-dialog>
         </v-dialog>
 
