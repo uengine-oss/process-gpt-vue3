4c76aef54035cc9f03eceb1a64fffc5f7ff54d30
m6023m <m6023m@naver.com>
2024-07-08 16:13:33 +0900

    fix process start error

--- src/components/customBpmn/index.js
+++ src/components/customBpmn/index.js
@@ -1,8 +1,6 @@
 import CustomBpmnRenderer from './CustomBpmnRenderer';
-import PaletteProvider from './PaletteProvider';
 
 export default {
-  __init__: [ 'customBpmnRenderer' , 'paletteProvider'],
-  customBpmnRenderer: [ 'type', CustomBpmnRenderer ],
-  paletteProvider: [ 'type', PaletteProvider ]
+  __init__: [ 'customBpmnRenderer'],
+  customBpmnRenderer: [ 'type', CustomBpmnRenderer ]
 };
