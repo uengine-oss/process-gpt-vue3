76875e53f138137a688feb950f8417c1a259ff75
dhtns818 <dhtns818@rosesystems.kr>
2024-07-15 18:04:06 +0900

    update process instance name pattern

--- src/components/designer/InstanceNamePatternForm.vue
+++ src/components/designer/InstanceNamePatternForm.vue
@@ -1,10 +1,8 @@
 <template>
-    <v-card flat>
-        <v-card-text>
-            <v-text-field label="Pattern" v-model="pattern"></v-text-field>
-            <div v-html="explanation"  style="max-height: 600px; overflow-y: scroll;"></div>
-        </v-card-text>
-    </v-card>
+    <div class="pa-2">
+        <v-text-field label="Pattern" v-model="pattern"></v-text-field>
+        <div v-html="explanation" class="px-2 pb-5"></div>
+    </div>
 </template>
 
 <script>
@@ -29,12 +27,16 @@ export default {
                 JSP 태그을 이용한 패턴 사용으로 인스턴스명의 작성이 가능합니다.
                 <br><br>
                 <h3> 1. instance ID 이용한 패턴 작성.</h3>
+                <span>
                 <b> 예시: uEgnine_<%=instance.instanceId%></b><br>
                 결과: uEgnine_13
+                </span>
                 <br><br>
                 <h3> 2. 해당 Role의 Scope정보 설정 </h3>
+                <span>
                 <b> 예시: uEgnine_<%=roles.사용자.endpoint%></b><br>
                 결과: uEgnine_manager
+                </span>
                 <br><br>
                 <h3> 3. 프로세스명 으로 설정 </h3>
                 <b> 예시: uEgnine_<%=definition.id%></b><br>
