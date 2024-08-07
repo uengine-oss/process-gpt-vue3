5512bb768bc7bf8874f753847a0f6eb9e7705a9d
m6023m <m6023m@naver.com>
2024-08-07 12:48:29 +0900

    add scroll in mapper

--- src/components/designer/bpmnModeling/bpmn/panel/ReceiveTaskPanel.vue
+++ src/components/designer/bpmnModeling/bpmn/panel/ReceiveTaskPanel.vue
@@ -117,6 +117,7 @@
             v-model="isOpenFieldMapper"
             max-width="80%"
             max-height="80%"
+            class="mapper-dialog"
             @afterLeave="$refs.mapper && $refs.mapper.saveFormMapperJson()"
         >
             <mapper
