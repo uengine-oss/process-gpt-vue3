fb31b46fa212d9e1684425c2c097b3ef59ac3663
m6023m <m6023m@naver.com>
2024-11-21 10:05:34 +0900

    Add cronTab in TimerEventDefinitionPanel

--- src/components/designer/bpmnModeling/bpmn/panel/EventPanel/TimerEventDefinitionPanel.vue
+++ src/components/designer/bpmnModeling/bpmn/panel/EventPanel/TimerEventDefinitionPanel.vue
@@ -1,27 +1,80 @@
 <template>
     <div>
         <!-- <cron-vuetify v-model="cron" :chip-props="{ color: 'success', textColor: 'white' }" @error="error = $event" /> -->
-        <v-text-field class="mt-4" v-model="copyUengineProperties.expression" :label="$t('TimerEventDefinitionPanel.cron')"></v-text-field>
+        <!-- <v-text-field class="mt-4" v-model="copyUengineProperties.expression" :label="$t('TimerEventDefinitionPanel.cron')"></v-text-field> -->
+        
+        <cron-core v-model="expression" format="quartz" v-slot="{fields, period, error}">
+            <div>
+
+                <!-- period selection -->
+                {{period.prefix}}
+                <v-chip>
+                {{period.attrs.modelValue}}
+                <v-menu activator="parent">
+                    <v-list>
+                    <v-list-item v-for="item in period.items"  :key="item.id" @click="period.events['update:model-value'](item.id)">
+                        {{item.text}}
+                    </v-list-item>
+                    </v-list>
+                </v-menu>
+                <v-icon small @click="period.events['update:model-value']('')">mdi-close</v-icon>
+                </v-chip>
+                {{period.suffix}}
+
+                <!-- cron expression fields -->
+                <template v-for="f in fields" :key="f.id">
+                {{f.prefix}}
+
+                    <v-chip>
+                    {{f.selectedStr}}
+                    <v-menu activator="parent" :close-on-content-click="false">
+
+                        <!-- list of field items -->
+                        <v-list :selected="f.attrs.modelValue" @update:selected="f.events['update:model-value']" select-strategy="multiple">
+                        <v-list-item v-for="item in f.items" :value="item.value" :key="item.value">
+                            {{item.text}}
+                        </v-list-item>
+                        </v-list>
+
+                    </v-menu>
+                    <v-icon small @click="f.events['update:model-value']([])">mdi-close</v-icon>
+                    </v-chip>
+                {{f.suffix}}
+                </template>
+
+                <!-- editable cron expression -->
+                <v-text-field
+                class="mt-4"
+                :modelValue="expression"
+                @update:model-value="nextValue = $event"
+                @blur="value = nextValue"
+                label="cron expression"
+                :error-messages="error" />
+
+            </div>
+        </cron-core>
         <DetailComponent
             :title="$t('TimerEventDefinitionPanel.cronDescriptionTitle')"
             :details="cronDescription"
         />
-        <!-- editable cron expression -->
     </div>
 </template>
 <script>
+import { CronCore } from '@vue-js-cron/core'
 import { useBpmnStore } from '@/stores/bpmn';
 import { Icon } from '@iconify/vue';
 // import { setPropeties } from '@/components/designer/bpmnModeling/bpmn/panel/CommonPanel.ts';
 export default {
     name: 'timer-event-definition-panel',
+    components: {
+        CronCore
+    },
     props: {
         element: Object,
         uengineProperties: Object,
         processDefinitionId: String,
         isViewMode: Boolean
     },
-    components: {},
     created() {
         // console.log(this.element.eventDefinitions);
         // if (this.element.eventDefinitions.length > 0) {
@@ -52,8 +105,7 @@ export default {
             paramKey: '',
             paramValue: '',
             eventType: null,
-            cron: '* * * * *',
-            nextCron: '* * * * *',
+            expression: '* * * * * *',
             error: '',
             cronDescription: [
                 {
@@ -62,89 +114,23 @@ export default {
             ],
         };
     },
-    async mounted() {
-        let me = this;
-
-        const store = useBpmnStore();
-        this.bpmnModeler = store.getModeler;
-        // Object.keys(this.requiredKeyLists).forEach((key) => {
-        //     this.ensureKeyExists(this.uengineProperties, key, this.requiredKeyLists[key]);
-        // });
+    mounted() {
+        if(!this.copyUengineProperties.expression) {
+            this.copyUengineProperties.expression = "* * * * * *" + " *";
+            this.$emit('update:uEngineProperties', this.copyUengineProperties);
+        } else {
+            this.expression = this.copyUengineProperties.expression.slice(0, -2);
+        }
     },
     computed: {
-        panelName() {
-            return _.kebabCase(this.eventType.split(':')[1]) + '-panel';
-        },
-        inputData() {
-            let params = this.copyUengineProperties.parameters;
-            let result = [];
-            if (params)
-                params.forEach((element) => {
-                    if (element.direction == 'IN') result.push(element);
-                });
-            return result;
-        },
-        outputData() {
-            let params = this.copyUengineProperties.parameters;
-            let result = [];
-            if (params)
-                params.forEach((element) => {
-                    if (element.direction == 'OUT') result.push(element);
-                });
-            return result;
+    },
+    watch: {
+        expression(newVal) {
+            this.copyUengineProperties.expression = newVal + " *";
+            this.$emit('update:uEngineProperties', this.copyUengineProperties);
         }
     },
-    watch: {},
     methods: {
-        deleteInputData(inputData) {
-            const index = this.copyUengineProperties.parameters.findIndex((element) => element.key === inputData.key);
-            if (index > -1) {
-                this.copyUengineProperties.parameters.splice(index, 1);
-                this.$emit('update:uEngineProperties', this.copyUengineProperties);
-            }
-        },
-        deleteOutputData(outputData) {
-            const index = this.copyUengineProperties.parameters.findIndex((element) => element.key === outputData.key);
-            if (index > -1) {
-                this.copyUengineProperties.parameters.splice(index, 1);
-                this.$emit('update:uEngineProperties', this.copyUengineProperties);
-            }
-        },
-        ensureKeyExists(obj, key, defaultValue) {
-            if (!obj.hasOwnProperty(key)) {
-                obj[key] = defaultValue;
-            }
-        },
-        deleteExtendedProperty(item) {
-            const index = this.copyUengineProperties.extendedProperties.findIndex((element) => element.key === item.key);
-            if (index > -1) {
-                this.copyUengineProperties.extendedProperties.splice(index, 1);
-                this.$emit('update:uEngineProperties', this.copyUengineProperties);
-            }
-        },
-        deleteCheckPoint(item) {
-            const index = this.copyUengineProperties.checkpoints.findIndex((element) => element.checkpoint === item.checkpoint);
-            if (index > -1) {
-                this.copyUengineProperties.checkpoints.splice(index, 1);
-                this.$emit('update:uEngineProperties', this.copyUengineProperties);
-            }
-        },
-        addParameter() {
-            this.copyUengineProperties.extendedProperties.push({ key: this.paramKey, value: this.paramValue });
-            this.$emit('update:uEngineProperties', this.copyUengineProperties);
-            // const bpmnFactory = this.bpmnModeler.get('bpmnFactory');
-            // // this.checkpoints.push(this.checkpointMessage)
-            // const parameter = bpmnFactory.create('uengine:ExtendedProperty', { key: this.paramKey, value: this.paramValue });
-            // if (!this.elementCopy.extensionElements.values[0].ExtendedProperties) this.elementCopy.extensionElements.values[0].ExtendedProperties = []
-            // this.elementCopy.extensionElements.values[0].ExtendedProperties.push(parameter)
-            // this.paramKey = ""
-            // this.paramValue = ""
-        },
-        
-        addCheckpoint() {
-            this.copyUengineProperties.checkpoints.push({ checkpoint: this.checkpointMessage.checkpoint });
-            this.$emit('update:uEngineProperties', this.copyUengineProperties);
-        }
     }
 };
 </script>
