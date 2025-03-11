e22e3a7b5ac414a77863d46a5486e1945d98f459
sooheon45 <sooheon45@uengine.org>
2025-03-11 11:56:05 +0900

    조건문 관련 오류 및 변수명 수정

--- src/components/designer/bpmnModeling/bpmn/panel/ConditionField.vue
+++ src/components/designer/bpmnModeling/bpmn/panel/ConditionField.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <draggable class="list-group cursor-pointer" :list="condition" :animation="0" ghost-class="ghost-card" group="condition">
-            <template v-for="(item, idx) in getConditions(condition)" :key="idx">
-                <div v-if="checkCondition(item)" class="d-flex condition-box mt-2">
+            <template v-for="(cond, idx) in filteredCondition" :key="idx">
+                <div v-if="checkCondition(cond)" class="d-flex condition-box mt-2">
                     <v-combobox
-                        :label="item.key"
-                        v-model="item.key"
+                        :label="cond.key"
+                        v-model="cond.key"
                         :items="varItems"
                         variant="outlined"
                         density="comfortable"
@@ -13,16 +13,16 @@
                     ></v-combobox>
 
                     <v-select
-                        :label="item.condition"
-                        v-model="item.condition"
+                        :label="cond.condition"
+                        v-model="cond.condition"
                         :items="conditionList"
                         class="mx-1"
                         style="max-width: 100px;"
                     ></v-select>
 
                     <v-combobox
-                        :label="item.value"
-                        v-model="item.value"
+                        :label="cond.value"
+                        v-model="cond.value"
                         :items="varItems"
                         variant="outlined"
                         density="comfortable"
@@ -37,20 +37,19 @@
                             <DotsVerticalIcon />
                             <v-menu activator="parent">
                                 <v-list>
-                                    <v-list-item v-for="menu in menuList" :key="menu" @click="changeCondition(item, menu)">
+                                    <v-list-item v-for="menu in menuList" :key="menu" @click="changeCondition(cond, menu)">
                                         <v-list-item-title>{{ menu }}</v-list-item-title>
                                     </v-list-item>
                                 </v-list>
                             </v-menu>
                         </v-btn>
-
-                        <v-btn v-if="idx == getConditions(condition).length - 1" icon variant="text" @click="addCondition(item)"
+                        <v-btn v-if="idx == filteredCondition.length - 1 && filteredCondition.length == 1" icon variant="text" @click="addCondition(idx)"
                             density="comfortable"
                             class="ml-1"
                         >
                             <v-icon>mdi-plus</v-icon>
                         </v-btn>
-                        <v-btn v-if="getConditions(condition).length > 1" icon variant="text" @click="deleteCondition(item, idx)"
+                        <v-btn v-if="filteredCondition.length > 1" icon variant="text" @click="deleteCondition(cond, idx)"
                             density="comfortable"    
                             class="ml-1"
                         >
@@ -60,16 +59,22 @@
                 </div>
                 <div v-else class="pa-3 border" style="border-radius:8px;">
                     <div class="d-flex">
-                        <v-chip>
-                            {{ getTypeShortName(item._type) }}
+                        <v-chip @click="changeConditionType(cond)" :color="getTypeColor(cond._type)" variant="flat">
+                            {{ getTypeShortName(cond._type) }}
                         </v-chip>
-                        <v-btn icon variant="text" class="ml-auto" @click="deleteCondition(item, idx)"
+                       
+                        <v-btn icon variant="text" class="ml-auto" @click="addCondition(idx)"
+                                    density="comfortable"
+                        >
+                            <v-icon>mdi-plus</v-icon>
+                        </v-btn>
+                        <v-btn icon variant="text" @click="deleteCondition(cond, idx)"
                             density="comfortable"
                         >
                             <Icons :icon="'trash'" />
                         </v-btn>
                     </div>
-                    <ConditionField :value="item.conditionsVt ? item.conditionsVt : item.condition" @update:value="(val) => updateChild(item, val)" />
+                    <ConditionField :value="cond.conditionsVt ? cond.conditionsVt : cond.condition" @update:value="(val) => updateChild(cond, val)" />
                 </div>
             </template>
         </draggable>
@@ -97,7 +102,7 @@ export default {
         condition: {
             deep: true,
             handler(val) {
-                console.log(val);
+                // console.log(val);
                 this.$emit('update:value', val);
             }
         }
@@ -113,6 +118,14 @@ export default {
                 condition: ''
             };
         }
+    },
+    computed:{
+        filteredCondition(){
+            if (Array.isArray(this.condition)) {
+               return this.condition;
+            }
+            return [this.condition]
+        },
     },
     mounted() {
         const bpmnStore = useBpmnStore();
@@ -133,29 +146,54 @@ export default {
     },
     methods: {
         updateChild(item, val) {
-            //
             if(item._type == 'org.uengine.kernel.Not')
             item.condition = val
+        },
+        changeConditionType(item){
+            if(item._type == 'org.uengine.kernel.And'){
+                item._type = 'org.uengine.kernel.Or';
+            } else if(item._type == 'org.uengine.kernel.Or'){
+                item._type = 'org.uengine.kernel.Not';
+            } else if(item._type == 'org.uengine.kernel.Not'){
+                item._type = 'org.uengine.kernel.And';
+            }
+        },
+        getTypeColor(type) {
+            if(type.includes('And')) {
+                return 'primary';
+            } else if(type.includes('Or')) {
+                return 'success';
+            } else if(type.includes('Not')) {
+                return 'error';
+            }
+            return 'grey';
         },
         changeCondition(item, type) {
             const child = JSON.parse(JSON.stringify(item));
             if (type == 'AND') {
-                item.conditionsVt = [];
-                item.conditionsVt.push(child);
                 item._type = 'org.uengine.kernel.And';
+
+                if(!item.conditionsVt) {
+                    item.conditionsVt = [];
+                    item.conditionsVt.push(child);
+                }
+
                 item.conditionsVt.push({
                     _type: 'org.uengine.kernel.Evaluate',
                     key: '',
                     value: '',
                     condition: ''
                 });
+
+               
                 delete item.key;
                 delete item.value;
                 delete item.condition;
             } else if (type == 'OR') {
+                item._type = 'org.uengine.kernel.Or';
+
                 item.conditionsVt = [];
                 item.conditionsVt.push(child);
-                item._type = 'org.uengine.kernel.Or';
                 item.conditionsVt.push({
                     _type: 'org.uengine.kernel.Evaluate',
                     key: '',
@@ -192,31 +230,37 @@ export default {
                 };
             }
         },
-        addCondition(item) {
+        addCondition(idx) {
             if (Array.isArray(this.condition)) {
-                this.condition.push({
+                let condition = this.condition[idx];
+                let child = {
                     _type: 'org.uengine.kernel.Evaluate',
                     key: '',
                     value: '',
                     condition: ''
-                });
+                }
+
+                if(condition.conditionsVt){
+                    condition.conditionsVt.push(child);
+                } else {
+                    this.condition.push(child);
+                }
             } else {
-                this.changeCondition(item, 'AND');
+                this.changeCondition(this.condition, 'AND');
             }
         },
         checkCondition(item) {
-            const condition = item._type.includes('Evaluate');
-            return condition;
+            return item._type.includes('Evaluate');
         },
-        getConditions(item) {
-            var result = [];
-            if (Array.isArray(item)) {
-                result = item;
-            } else {
-                result.push(item);
-            }
-            return result;
-        },
+        // getConditions(item) {
+        //     var result = [];
+        //     if (Array.isArray(item)) {
+        //         result = item;
+        //     } else {
+        //         result.push(item);
+        //     }
+        //     return result;
+        // },
         getTypeShortName(type) {
             const parts = type.split('.');
             return parts[parts.length - 1];
