37ed537a61916de87fe213a0f4cf391f916bb161
dhtns818 <dhtns818@rosesystems.kr>
2025-03-20 15:16:12 +0900

    fix routing issues

--- src/router/index.ts
+++ src/router/index.ts
@@ -61,45 +61,45 @@ router.beforeEach(async (to, from, next) => {
                 });
             }
         }
-    }
-
-    // redirect to login page if not logged in and trying to access a restricted page
-    const publicPages = ['/'];
-    const authRequired = !publicPages.includes(to.path);
-    let isLogin = false;
-    if (window.$isTenantServer) {
-        isLogin = await backend.checkDBConnection();
-    } else {
-        isLogin = await backend.setTenant(window.$tenantName) ?? false;
-    }
-
-    if (to.matched.some((record) => record.meta.requiresAuth)) {
-        if (authRequired && !isLogin) {
-            if(window.location.pathname == '/auth/register'){
-                alert("계정 인증 메일을 전송해드렸습니다. 이메일 확인 후 다시 로그인하세요.");
-            } else {
-                alert("로그인이 필요합니다.")
-            }
-            return next('/auth/login');
-        }
-    }
-
-    if (to.fullPath.includes('/auth')) {
-        next();
-    } else {
+        // redirect to login page if not logged in and trying to access a restricted page
+        const publicPages = ['/'];
+        const authRequired = !publicPages.includes(to.path);
+        let isLogin = false;
         if (window.$isTenantServer) {
-            if (!to.fullPath.includes('/tenant') && to.fullPath !== '/') {
-                return next('/tenant/manage');
-            } else if (to.fullPath === '/' || 
-                to.matched.some((record) => TenantRoutes.children.includes(record as any))) {
-                next();
-            }
+            isLogin = await backend.checkDBConnection();
         } else {
-            if (to.fullPath.includes('/tenant')) {
-                return next('/');
+            isLogin = await backend.setTenant(window.$tenantName) ?? false;
+        }
+
+        if (to.matched.some((record) => record.meta.requiresAuth)) {
+            if (authRequired && !isLogin) {
+                if(window.location.pathname == '/auth/register'){
+                    alert("계정 인증 메일을 전송해드렸습니다. 이메일 확인 후 다시 로그인하세요.");
+                } else {
+                    alert("로그인이 필요합니다.")
+                }
+                return next('/auth/login');
+            }
+        }
+
+        if (to.fullPath.includes('/auth')) {
+            next();
+        } else {
+            if (window.$isTenantServer) {
+                if (!to.fullPath.includes('/tenant') && to.fullPath !== '/') {
+                    return next('/tenant/manage');
+                } else if (to.fullPath === '/' || 
+                    to.matched.some((record) => TenantRoutes.children.includes(record as any))) {
+                    next();
+                }
+            } else {
+                if (to.fullPath.includes('/tenant')) {
+                    return next('/');
+                }
             }
         }
     }
+    
     next();
 });
 
