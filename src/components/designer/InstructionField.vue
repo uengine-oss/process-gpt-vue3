fc19ec1bc9c1c613ddd2bc67a1bcad7535e2be66
jhyg <jaeh@uengine.org>
2025-06-09 16:52:21 +0900

    fix error

--- src/components/designer/InstructionField.vue
+++ src/components/designer/InstructionField.vue
@@ -11,7 +11,7 @@ export default {
     },
     data() {
         return {
-            instruction: JSON.parse(JSON.stringify(this.modelValue)),            
+            instruction: this.modelValue ? JSON.parse(JSON.stringify(this.modelValue)) : '',            
         };
     },
 
