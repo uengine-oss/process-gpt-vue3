0e32a85b0577f1f2255d7168f0dbdadc26c77062
kibumpark <top5w5@naver.com>
2025-06-18 15:10:29 +0900

    모바일에서 채팅방 클릭시 닫히도록 수정, 왼쪽 사이드바 공백 제거, 프로젝트 등록 UI 수정, 모바일 환경에서 프로세스 정의 체계도 UI 수정 일부 접속 국가별 언어 설정

--- src/components/apps/todolist/ProjectCreationForm.vue
+++ src/components/apps/todolist/ProjectCreationForm.vue
@@ -1,18 +1,26 @@
 <template>
     <div>
         <v-card>
-            <v-card-title  class="pt-5 pl-5">
-                <h4 class="text-h4">프로젝트 생성</h4>
+            <v-card-title class="pt-5 pl-5 d-flex align-center">
+                <h4 class="text-h4">{{ $t('ProjectCreationForm.title') }}</h4>
+                <v-spacer></v-spacer>
+                <v-btn icon variant="text" @click="close" class="ml-auto">
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
             </v-card-title>
-            <v-card-text>
+            <v-card-text class="pa-0 pl-4 pr-4 pb-4">
                 <v-text-field v-model="project.name" label="프로젝트 명" autofocus required style="margin-bottom: 10px;"></v-text-field>
                 <v-text-field v-model="project.startDate" label="시작일" type="datetime-local" required style="margin-bottom: 10px;"></v-text-field>
                 <v-text-field v-model="project.dueDate" label="마감일" type="datetime-local" required></v-text-field>
             </v-card-text>
 
-            <v-card-actions class="justify-center pt-0">
-                <v-btn :disabled="project.name == ''" color="primary" variant="flat" @click="save">생성</v-btn>
-                <v-btn color="error" variant="flat" @click="close">닫기</v-btn>
+            <v-card-actions class="justify-end pt-0">
+                <v-btn @click="save"
+                    :disabled="project.name == ''"
+                    color="primary"
+                    variant="flat"
+                    rounded
+                >{{ $t('ProjectCreationForm.create') }}</v-btn>
             </v-card-actions>
         </v-card>
     </div>
