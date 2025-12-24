bed152d51d0bfe8a869edf95740c5c5d68e6e937
kibum0405 <top5w5@naver.com>
2025-12-24 14:16:39 +0900

    배포된 사이트에서 스크롤 확인 콘솔 추가

--- src/components/customZoomScroll/customZoomScroll.js
+++ src/components/customZoomScroll/customZoomScroll.js
@@ -37,14 +37,18 @@ export default function CustomZoomScroll(config, eventBus, canvas) {
   var self = this;
   var container = canvas._container;
   
+  console.log('[줌스크롤] 초기화 시작, container:', container);
+  
   if (container) {
     var handleWheelWithPassiveFalse = function(event) {
+      console.log('[줌스크롤] wheel 이벤트 발생');
       self._handleWheel(event);
     };
     
     // 부모 클래스가 등록한 이벤트를 제거하고 passive: false로 재등록
     setTimeout(function() {
       container.addEventListener('wheel', handleWheelWithPassiveFalse, { passive: false, capture: true });
+      console.log('[줌스크롤] wheel 이벤트 등록 완료 (passive: false, capture: true)');
     }, 0);
   }
 
@@ -59,14 +63,21 @@ CustomZoomScroll.$inject = [
 CustomZoomScroll.prototype = Object.create(ZoomScroll.prototype);
 
 CustomZoomScroll.prototype.zoom = function(delta, position) {
+  console.log('[줌스크롤] zoom 함수 호출됨, delta:', delta, 'position:', position);
+  
   var stepSize = getStepSize(this._customZoomRange, 10 * 2);
 
   this._totalDelta += delta;
 
+  console.log('[줌스크롤] _totalDelta:', this._totalDelta, '절댓값:', Math.abs(this._totalDelta));
+
   if (Math.abs(this._totalDelta) > 0.1) {
+    console.log('[줌스크롤] _zoom 실행');
     this._zoom(delta, position, stepSize);
 
     this._totalDelta = 0;
+  } else {
+    console.log('[줌스크롤] delta가 0.1 이하라서 _zoom 실행 안됨');
   }
 };
 
@@ -75,6 +86,8 @@ CustomZoomScroll.prototype.getStepSize = function() {
 };
 
 CustomZoomScroll.prototype._zoom = function(delta, position, stepSize) {
+  console.log('[줌스크롤] _zoom 함수 시작');
+  
   const canvas = this._canvas;
 
   const direction = delta > 0 ? 1 : -1;
@@ -87,28 +100,39 @@ CustomZoomScroll.prototype._zoom = function(delta, position, stepSize) {
 
   let newLogZoomLevel = Math.pow(10, newLinearZoomLevel);
 
+  console.log('[줌스크롤] 줌 레벨 변경:', canvas.zoom(), '->', cap(this._customZoomRange, newLogZoomLevel));
+  
   canvas.zoom(cap(this._customZoomRange, newLogZoomLevel), position);
+  
+  console.log('[줌스크롤] canvas.zoom 호출 완료');
 };
 
 
 
 CustomZoomScroll.prototype._handleWheel = function handleWheel(event) {
 
+  console.log('[줌스크롤] _handleWheel 호출됨');
+
   // event is already handled by '.djs-scrollable'
   if (domClosest(event.target, '.djs-scrollable', true)) {
+    console.log('[줌스크롤] djs-scrollable에서 이미 처리됨, 종료');
     return;
   }
 
   var element = this._container;
   var me = this;
 
+  console.log('[줌스크롤] preventDefault 호출 전, event:', event);
   event.preventDefault();
+  console.log('[줌스크롤] preventDefault 호출 완료');
 
   // pinch to zoom is mapped to wheel + ctrlKey = true
   // in modern browsers (!)
 
   // 마우스 휠 기본 동작을 줌으로 설정, Shift 키를 누르면 스크롤
   var isZoom = !event.shiftKey;
+  
+  console.log('[줌스크롤] isZoom:', isZoom, 'shiftKey:', event.shiftKey);
 
   var isHorizontalScroll = event.shiftKey;
 
@@ -122,6 +146,8 @@ CustomZoomScroll.prototype._handleWheel = function handleWheel(event) {
   }
 
   if (isZoom) {
+    console.log('[줌스크롤] 줌 모드 진입, deltaY:', event.deltaY);
+    
     var elementRect = element.getBoundingClientRect();
 
     var offset = {
@@ -136,9 +162,14 @@ CustomZoomScroll.prototype._handleWheel = function handleWheel(event) {
       ) * sign(event.deltaY) * factor
     );
 
+    console.log('[줌스크롤] 계산된 delta:', delta, 'offset:', offset);
+    
     // zoom in relative to diagram {x,y} coordinates
     this.zoom(delta, offset);
+    
+    console.log('[줌스크롤] zoom 함수 호출 완료');
   } else {
+    console.log('[줌스크롤] 스크롤 모드 진입');
 
     if (isHorizontalScroll) {
       delta = {
