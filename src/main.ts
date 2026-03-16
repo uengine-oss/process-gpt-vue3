a0daebd340ba9e78497530fd5bcc788f6cf36b91
kibum0405 <top5w5@naver.com>
2026-03-16 17:27:22 +0900

    퍼펙트 스크롤 의존성 관련 빌드 오류 수정

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import VueTablerIcons from 'vue-tabler-icons';
 import VueApexCharts from 'vue3-apexcharts';
 import 'vue3-carousel/dist/carousel.css';
 import { PerfectScrollbar } from 'vue3-perfect-scrollbar';
-import 'vue3-perfect-scrollbar/style.css';
+import 'perfect-scrollbar/css/perfect-scrollbar.css';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import hammerDirective from '@/components/directive/hammerDirective';
